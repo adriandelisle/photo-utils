161b84bc31c99e1977ff3ff3b7d1fd6bc98b4f80
Adrian De Lisle <Adrian De Lisle>
2016-11-27 22:43:41 -0800

    added a bytes to human readable function

--- photoUtils/utils.py
+++ photoUtils/utils.py
@@ -1,6 +1,37 @@
 def is_picture(fileName):
-    acceptedFileTypes = ['.jpg', '.nef']
+    acceptedFileTypes = ['.jpg', '.nef', '.dng']
     for fileType in acceptedFileTypes:
         if fileName.lower().endswith(fileType):
             return True
     return False
+
+# http://stackoverflow.com/a/37423778
+def bytes_2_human_readable(number_of_bytes):
+    if number_of_bytes < 0:
+        raise ValueError("!!! numberOfBytes can't be smaller than 0 !!!")
+
+    step_to_greater_unit = 1024.
+
+    number_of_bytes = float(number_of_bytes)
+    unit = 'bytes'
+
+    if (number_of_bytes / step_to_greater_unit) >= 1:
+        number_of_bytes /= step_to_greater_unit
+        unit = 'KB'
+
+    if (number_of_bytes / step_to_greater_unit) >= 1:
+        number_of_bytes /= step_to_greater_unit
+        unit = 'MB'
+
+    if (number_of_bytes / step_to_greater_unit) >= 1:
+        number_of_bytes /= step_to_greater_unit
+        unit = 'GB'
+
+    if (number_of_bytes / step_to_greater_unit) >= 1:
+        number_of_bytes /= step_to_greater_unit
+        unit = 'TB'
+
+    precision = 1
+    number_of_bytes = round(number_of_bytes, precision)
+
+    return str(number_of_bytes) + ' ' + unit
---
+++ timelapse_stats.py
@@ -0,0 +1,68 @@
+import os
+import time
+from multiprocessing import Pool
+from datetime import timedelta
+import photoUtils
+
+def process_directory(root, photoDir):
+    fullDir = os.path.join(root, photoDir)
+    print ("Processing: " + fullDir)
+    
+    dirStats = {'numberOfFiles': 0, 'duration': timedelta(0), 'size': 0}
+    maybeTimelapseDirectory = os.path.join(fullDir, 'Timelapse')
+    if os.path.isdir(maybeTimelapseDirectory):
+        print ("\tProcessing timelapse directory: " + maybeTimelapseDirectory)
+        files = os.listdir(maybeTimelapseDirectory)
+        sortedFiles = sorted(files)
+
+        photoFiles = []
+        for file in sortedFiles:
+            if photoUtils.utils.is_picture(file):
+                filePath = os.path.join(maybeTimelapseDirectory, file)
+                photoFiles.append(filePath)
+                dirStats['size'] += os.path.getsize(filePath)
+                dirStats['numberOfFiles'] += 1
+        firstPhotoMetadata = photoUtils.metadata.get_metadata(photoFiles[0])
+        firstTimestamp = photoUtils.metadata.get_created_time(firstPhotoMetadata)
+
+        lastPhotoMetadata = photoUtils.metadata.get_metadata(photoFiles[-1])
+        lastTimestamp = photoUtils.metadata.get_created_time(lastPhotoMetadata)
+
+        dirStats['duration'] = lastTimestamp - firstTimestamp
+    print ('\tDirectory stats for: ' + fullDir + '\n\t' + str(dirStats))
+    return dirStats
+
+def print_dirStats(dirStats):
+    print ('Number of files: ' + str(dirStats['numberOfFiles']) + '\n' + \
+           'Duration: ' + str(dirStats['duration']) + '\n' + \
+           'Size: ' + photoUtils.utils.bytes_2_human_readable(dirStats['size']))
+
+def merge_processingResults(processingResults):
+    dirStats = {'numberOfFiles': 0, 'duration': timedelta(0), 'size': 0}
+    for result in processingResults:
+        dirStats['numberOfFiles'] += result['numberOfFiles']
+        dirStats['duration'] = dirStats['duration'] + result['duration']
+        dirStats['size'] += result['size']
+    return dirStats
+
+def main ():
+    directories = ['P:/Pictures/Nikon D5000 Photos/Timelapses/']
+    #directories = ['P:/Pictures/Python Test/Timelapses/']
+
+    start = time.time()
+    processingResults = []
+    for directoryToCheck in directories:
+        for photoDir in os.listdir(directoryToCheck):
+            processingResults.append(process_directory(directoryToCheck, photoDir))
+##        # get the arguments for the procssing function
+##        photoDirectories = list(map(lambda photoDir: (directoryToCheck, photoDir), os.listdir(directoryToCheck)))
+##        with Pool(8) as p:
+##            processingResults.extend(p.starmap(process_directory, photoDirectories))
+    end = time.time()
+
+    totalDirStats = merge_processingResults(processingResults)
+    print_dirStats(totalDirStats)
+    print ("Time taken: " + str(end - start))
+        
+if __name__ == "__main__":
+    main()
