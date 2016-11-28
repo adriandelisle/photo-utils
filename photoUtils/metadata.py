f5270fc2bc40c08db4a38f08ad249740f6356259
Adrian De Lisle <Adrian De Lisle>
2016-11-27 21:52:23 -0800

    added better handling of metadata and a get_created_time

--- photoUtils/metadata.py
+++ photoUtils/metadata.py
@@ -1,4 +1,6 @@
 import subprocess
+import time
+from datetime import datetime, timedelta
 from pkg_resources import resource_filename
 
 exiv2Path = resource_filename(__name__, 'exiv2-0.25-win/exiv2.exe')
@@ -16,8 +18,8 @@ def get_metadata(filePath):
     metadata = {}
     for field in rawMetadata:
         keyValue = field.split(':')
-        if len(keyValue) == 2:
-            metadata[keyValue[0].strip()] = keyValue[1].strip()
+        if len(keyValue) >= 2:
+            metadata[keyValue[0].strip()] = ":".join(keyValue[1:]).strip()
     return metadata
 
 def get_aspect_ratio(metadata):
@@ -26,3 +28,7 @@ def get_aspect_ratio(metadata):
     imageSize = (float(imageSize[0].strip()), float(imageSize[1].strip()))
     
     return "{:.2f}".format(imageSize[0] / imageSize[1])
+
+def get_created_time(metadata):
+    metadataTimestamp = metadata['Image timestamp']
+    return datetime.fromtimestamp(time.mktime(time.strptime(metadataTimestamp, "%Y:%m:%d %H:%M:%S")))
